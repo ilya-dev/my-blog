e10ea94e50ec97515dfa3940e70ef6a6bc3abdcd
Ilya S <public.ilya.s@gmail.com>
2014-04-28 13:16:10 +0700

    improve look (tags on /posts/{id})

--- blog/app/views/posts/show.blade.php
+++ blog/app/views/posts/show.blade.php
@@ -5,9 +5,16 @@
 @section('content')
     @include('partials.post')
     <div id="tags">
+        Tags:
         @foreach ($post->tags as $tag)
             <a href="/tags/{{ $tag->id }}">{{ $tag->name }}</a>
         @endforeach
     </div>
+
+    <style>
+        #tags {
+            font-size: 24px;
+        }
+    </style>
 @stop
 
